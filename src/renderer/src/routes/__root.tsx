10884febaf333f412e0878195aeb50d075c1ce70
yashjawale <yash25.j@gmail.com>
2026-01-31 14:35:31 +0530

    feat: add variable font support with @fontsource-variable/inter and update CSS font settings

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -2,6 +2,8 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { SidebarProvider, SidebarTrigger } from '@renderer/components/ui/sidebar'
 import { AppSidebar } from '@renderer/components/AppSidebar'
+// Supports weights 100-900
+import '@fontsource-variable/inter'
 
 const RootLayout = () => (
   <>
